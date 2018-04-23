973bed89520179004ec0e2208246d00191cead1e
KlejdyszS <grawerjk@gmail.com>
2018-04-23 19:44:19 +0200

    Register and login v1

---
+++ www/js/login.js
@@ -0,0 +1,79 @@
+function loginFirebase() {
+	 $('#register').click(function() {
+
+			var email = $('#userEmail');    
+			var pass = $('#userPass');      
+
+						if(email.val() && pass.val()){
+
+				firebase.auth().createUserWithEmailAndPassword(email.val(), pass.val()).then(function(user){
+					console.log('everything went fine');
+					console.log('user object:' + user);
+					//you can save the user data here.
+				}).catch(function(error) {
+					console.log('there was an error');
+					var errorCode = error.code;
+					var errorMessage = error.message;
+					console.log(errorCode + ' - ' + errorMessage);
+				});
+
+			} else {
+				console.log('fill in both fields');
+			}  
+		});
+		
+		 $('#login').click(function() {
+
+			var email = $('#userEmail');    
+			var pass = $('#userPass');      
+
+						if(email.val() && pass.val()){
+
+				firebase.auth().signInWithEmailAndPassword(email.val(), pass.val()).then(function(user){
+					console.log('everything went fine');
+					console.log('user object:' + user);
+					//you can save the user data here.
+				}).catch(function(error) {
+					console.log('there was an error');
+					var errorCode = error.code;
+					var errorMessage = error.message;
+					console.log(errorCode + ' - ' + errorMessage);
+				});
+
+			} else {
+				console.log('fill in both fields');
+			}  
+		});
+		
+}  
+
+function loginFirebaseStatus() {
+	firebase.auth().onAuthStateChanged(function(user) {
+		  if (user) {
+			// User is signed in.
+			var displayName = user.displayName;
+			var email = user.email;
+			var emailVerified = user.emailVerified;
+			var photoURL = user.photoURL;
+			var isAnonymous = user.isAnonymous;
+			var uid = user.uid;
+			var providerData = user.providerData;
+			console.log(displayName);
+				console.log(email);
+				console.log(user.emailVerified);
+				$( "#loggedas" ).append('zalogowany');
+				
+		  } else {
+			console.log('user signed out');
+		  }
+		});
+		
+}  
+
+function init() {
+	loginFirebase();
+	loginFirebaseStatus();
+}
+
+
+
