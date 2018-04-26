d0f762de1757c56af021697bb4ba2c5903339e98
KlejdyszS <grawerjk@gmail.com>
2018-04-26 23:32:47 +0200

    Register and login v1.4.1

--- www/js/login.js
+++ www/js/login.js
@@ -1,3 +1,8 @@
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+
+
 function loginFirebase() {
 	 $('#register').click(function() {
 
@@ -45,6 +50,25 @@ function loginFirebase() {
 			}  
 		});
 		
+		$('#logingoogle').click(function() {
+		firebase.auth().signInWithRedirect(provider).then(function() {
+		  return firebase.auth().getRedirectResult();
+		}).then(function(result) {
+		  // This gives you a Google Access Token.
+		  // You can use it to access the Google API.
+		  var token = result.credential.accessToken;
+		  // The signed-in user info.
+		  var user = result.user;
+		  // ...
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		});
+	});
+
+		
+		
 		$('#logout').click(function() {
 
 			firebase.auth().signOut().then(function() {
@@ -54,8 +78,6 @@ function loginFirebase() {
 			});
 		
 		});
-		
-		
 		
 		
 }  
