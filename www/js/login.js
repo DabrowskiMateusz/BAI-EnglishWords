c213b17014ce084c3042e63edf4f7952353f9ea4
KlejdyszS <grawerjk@gmail.com>
2018-04-27 20:29:43 +0200

    Register and login v1.6.1

--- www/js/login.js
+++ www/js/login.js
@@ -66,7 +66,7 @@ function loginFirebase() {
 		  var errorMessage = error.message;
 		console.log(errorCode + ' - ' + errorMessage)
 		});
-	});
+		});
 
 		
 		
@@ -106,43 +106,6 @@ function loginFirebaseStatus() {
 		
 }  
 
-
-function onSignIn(googleUser) {
-  console.log('Google Auth Response', googleUser);
-  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-  var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
-    unsubscribe();
-    // Check if we are already signed-in Firebase with the correct user.
-    if (!isUserEqual(googleUser, firebaseUser)) {
-      // Build Firebase credential with the Google ID token.
-      var credential = firebase.auth.GoogleAuthProvider.credential(
-          googleUser.getAuthResponse().id_token);
-      // Sign in with credential from the Google user.
-      firebase.auth().signInWithCredential(credential).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
-    } else {
-      console.log('User already signed-in Firebase.');
-    }
-  });
-}
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-}
-
-
-
 function isUserEqual(googleUser, firebaseUser) {
   if (firebaseUser) {
     var providerData = firebaseUser.providerData;
@@ -157,8 +120,6 @@ function isUserEqual(googleUser, firebaseUser) {
   return false;
 }
 
-
-  
 
 
 function init() {
