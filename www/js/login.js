8b3fd096bb17857a049e439e0777c5a75a39fdfe
KlejdyszS <grawerjk@gmail.com>
2018-04-28 09:29:24 +0200

    Register and login v1.7.3

--- www/js/login.js
+++ www/js/login.js
@@ -135,47 +135,10 @@ function loginFirebaseStatus() {
 		  }
 			});
 			
-			facebookConnectPlugin.getLoginStatus(
-         function (status) {
-                alert("current status: " + JSON.stringify(status));
-
-                if (status.status == "connected") {
-
-                    facebookConnectPlugin.logout(function(){
-
-                        alert("LOGOUT SUCCESS");
-
-                        facebookConnectPlugin.login(["public_profile"],
-                                fbLoginSuccess,
-                                function (error) { alert("" + error) }
-                            );
-
-
-                    },function(){
-
-                        alert("LOGOUT FAIL");
-
-                    }); 
-
-
-                }
-                else{
-                     facebookConnectPlugin.login(["public_profile"],
-                                fbLoginSuccess,
-                                function (error) { alert("" + error) }
-                     );
-                }
-         },
-         function (e) {
-                alert("Failed: " + e);
-        });
-
-
-	var fbLoginSuccess = function (userData) {
-		alert("UserInfo: " + JSON.stringify(userData));
-		console.log("UserInfo: "+JSON.stringify(userData));
-
-	}
+			facebookConnectPlugin.getLoginStatus(function (status) {
+                console.log("current status: " + JSON.stringify(status));
+         });
+		 	
 }  
  
   
