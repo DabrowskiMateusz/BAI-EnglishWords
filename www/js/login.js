23f7a2b7c503d3bd80e89788d69430e7b5ab01a2
xotlanc <xotlanc@gmail.com>
2018-05-04 18:17:57 +0200

    fixed bug

--- www/js/login.js
+++ www/js/login.js
@@ -179,7 +179,9 @@ function loginFirebaseStatus() {
 		  }
 			});
 			
-			
+			facebookConnectPlugin.getLoginStatus(function (status) {
+                console.log("current status: " + JSON.stringify(status));
+         });
 		 	
 }  
   
