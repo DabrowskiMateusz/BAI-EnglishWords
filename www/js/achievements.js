94c62faa8a94c02af5ed72bd42d80ada4fbeefdc
KlejdyszS <grawerjk@gmail.com>
2018-05-08 19:33:21 +0200

    Achevements module

--- www/js/achievements.js
+++ www/js/achievements.js
@@ -10,13 +10,29 @@ function loadAnswers() {
 }
 
 function appendAnswers(path){
+	
+		var elem = document.getElementById("myBar"); 
+		var width = 1;
+		var id = setInterval(frame, 10);
+		function frame() {
+			if (width >= 100) {
+				clearInterval(id);
+			} else {
+				width++; 
+				elem.style.width = width + '%'; 
+			}
+		}
+	
+
+				
+
 	var correctAnswers = 0;
 	var inCorrectAnswers = 0;
 	var allAnswers = 0;
 	var refAnswers = getFromDb(path);
 	refAnswers.on("value", function(snapshot) {
 				snapshot.forEach(element => {
-					$( "#Answerstable" ).append("<tr class =\"wordsrow\"> <td>" + element.val().answer + "</td> <td>"
+					$( "#answerstable" ).append("<tr class =\"wordsrow\"> <td>" + element.val().answer + "</td> <td>"
 				+ element.val().word + "</td>  <td> <button onclick=\"deleteWord('"+element.key+"');\"> usuń </button> </td> </tr> ");
 				
 				if(element.val().answer == true){
@@ -26,12 +42,15 @@ function appendAnswers(path){
 				}
 					allAnswers += 1;
 				});
-			  }, function (errorObject) {
+				console.log("correct:" + correctAnswers + "incorrect:" + inCorrectAnswers + "all:" + allAnswers);
+							
+				
+				
+			  } , function (errorObject) {
 				console.log("The read failed: " + errorObject.code);
-			  });
-			  
-			  console.log("correct:" + correctAnswers + "incorrect:" + inCorrectAnswers + "all:" + allAnswers);
+			  });	    
 }
 
 
+  
 
