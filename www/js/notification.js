59cdfaaf45d0842a6f3241f5f29ba77c525a7714
KlejdyszS <grawerjk@gmail.com>
2018-05-09 21:10:22 +0200

    Notification changes

--- www/js/notification.js
+++ www/js/notification.js
@@ -3,20 +3,40 @@ function onDeviceReady() {
 	
 	
 }
+ $.fn.nval = function() {
+   return Number(this.val())
+};
 
-var now             = new Date().getTime(),
-    _5_sec_from_now = new Date(now + 5*1000);
+	var d = new Date();
+	var y = d.getFullYear();
+	var m = d.getMonth();
+	var today = d.getDate();
+	var year = y; 
+	var month = m;  
+	var day =  today;
+	
+	
 	
 function notification() {
-	console.log(now);
-	console.log(_5_sec_from_now);
-	
-    cordova.plugins.notification.local.schedule({
-        title: 'Słówko na dziś:',
-        text: 'Thats pretty easy...',
-        foreground: true,
-		trigger: { every: 'day', count: 5 },
+	var hours = ($( "#selectedHour" ).val());
+	var howoften =  (($( "#selectedHowOften" ).val()) * 60);
+	var date = new Date(year, month, day, hours);
+	var dateNow = new Date();
+		console.log(year);
+		console.log(month);
+		console.log(day);
+		console.log(date);
+		console.log(hours);
+		console.log(howoften);
+		console.log(dateNow);
+		cordova.plugins.notification.local.schedule({
+		id: 1,
+		title: "Message Title",
+		message: "Message Text",
+		firstAt: date, // firstAt and at properties must be an IETF-compliant RFC 2822 timestamp
+		every: howoften, // this also could be minutes i.e. 25 (int)
+		data: { meetingId:"123#fg8" }
+});
 
-    });
 
 }
